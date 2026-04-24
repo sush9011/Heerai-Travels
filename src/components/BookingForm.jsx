56803163b03fb4ce3c49318febb596cffbbee518
sush9011 <chavansushant6542@gmail.com>
2026-04-24 23:44:22 +0530

    Fixed Form Field

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -227,7 +227,7 @@ export default function BookingForm() {
               className={inputStyle}
             />
 
-            <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
               <input
                 type="date"
                 value={form.date}
